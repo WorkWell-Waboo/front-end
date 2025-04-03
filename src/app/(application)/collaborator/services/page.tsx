37b90a82db33061998f49a78f2a92bddebed83f6
maycoantony <marcosantonio50050@gmail.com>
2025-04-02 20:26:23 -0400

    commit: automatic

--- src/app/(application)/collaborator/services/page.tsx
+++ src/app/(application)/collaborator/services/page.tsx
@@ -1,9 +1,25 @@
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { HeaderCogSVG } from '@/assets/svgs/header-cog';
+import CardProfissional from '@/components/cardProfission';
+
 function CollaboratorServicesPage() {
-	return (
-		<main>
-			<h1>Serviços</h1>
-		</main>
-	);
+  return (
+    <div className="flex flex-1 gap-2 p-10">
+      <Card className="p-10">
+        <CardContent className="flex items-center justify-center">
+          <Avatar>
+            <AvatarFallback>
+              <HeaderCogSVG className="text-[#736CCE]" />
+            </AvatarFallback>
+          </Avatar>
+        </CardContent>
+        <CardFooter className="">
+          <p>Psicologia</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
 export default CollaboratorServicesPage;
