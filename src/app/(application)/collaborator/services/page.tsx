d8cb277fff474462ecee40accba9a2a86619fa18
maycoantony <marcosantonio50050@gmail.com>
2025-04-04 15:36:50 -0400

    commit: automatic

--- src/app/(application)/collaborator/services/page.tsx
+++ src/app/(application)/collaborator/services/page.tsx
@@ -1,13 +1,5 @@
 'use client';
-import AconselhamentoFinanceiroSVG from '../../../../assets/svgs/aconselhamento-financeiro';
-import FisioterapiaSVG from '../../../../assets/svgs/fisioterapia';
-import HealthCoachingSVG from '../../../../assets/svgs/health-coaching';
-import MassagemSVG from '../../../../assets/svgs/massagem';
-import NutricaoSVG from '../../../../assets/svgs/nutricion';
-import PsicologiaSVG from '../../../../assets/svgs/psicologia';
-import PsiquiatriaSVG from '../../../../assets/svgs/psiquiatria';
-import SonoSVG from '../../../../assets/svgs/sono';
-
+import parkOutlineSVG from '@/assets/svgs/park-outline';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
@@ -18,7 +10,14 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { useState } from 'react';
-
+import AconselhamentoFinanceiroSVG from '../../../../assets/svgs/aconselhamento-financeiro';
+import FisioterapiaSVG from '../../../../assets/svgs/fisioterapia';
+import HealthCoachingSVG from '../../../../assets/svgs/health-coaching';
+import MassagemSVG from '../../../../assets/svgs/massagem';
+import NutricaoSVG from '../../../../assets/svgs/nutricion';
+import PsicologiaSVG from '../../../../assets/svgs/psicologia';
+import PsiquiatriaSVG from '../../../../assets/svgs/psiquiatria';
+import SonoSVG from '../../../../assets/svgs/sono';
 interface Service {
   name: string;
   hasService: boolean;
@@ -33,16 +32,17 @@ const serviceIcons: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
   Massagem: MassagemSVG,
   Fisioterapia: FisioterapiaSVG,
   Sono: SonoSVG,
+  'park Outline': parkOutlineSVG,
 };
 const services: Service[] = [
   { name: 'Psicologia', hasService: true },
   { name: 'Psiquiatria', hasService: true },
   { name: 'Nutrição', hasService: true },
-  { name: 'Health Coaching', hasService: false },
+  { name: 'Health Coaching', hasService: true },
   { name: 'Aconselhamento Financeiro', hasService: true },
-  { name: 'Massagem', hasService: false },
-  { name: 'Fisioterapia', hasService: true },
-  { name: 'Sono', hasService: false },
+  { name: 'Massagem', hasService: true },
+  { name: 'Fisioterapia', hasService: false },
+  { name: 'Sono', hasService: true },
 ];
 
 export default function ServicesGrid() {
@@ -88,31 +88,61 @@ function ClickableCard({
     <>
       <Card
         data-status={status}
-        className={`p-10 cursor-pointer group 
+        className={`p-10 cursor-pointer group text-center text-[#4F4F4F]
           ${status === 'active' ? 'bg-primary text-white' : ''} 
           ${
             status === 'disabled'
-              ? 'bg-gray-400 text-gray-700 cursor-not-allowed'
+              ? 'bg-[#D3D3D3] text-[#585858]! cursor-not-allowed'
               : ''
           }`}
         onClick={handleClick}
       >
         <CardContent className="flex flex-col items-center justify-center">
           <Avatar>
-            <AvatarFallback className="bg-accent">
+            <AvatarFallback
+              data-status={status}
+              className={` bg-accent 
+                    ${status === 'active' ? 'text-primary! bg-[#F9F3FF]!' : ''} 
+                    ${status === 'disabled' ? ' bg-[#E7E7E7]! ' : ''}`}
+            >
               {IconComponent ? (
-                <IconComponent className="text-primary" />
+                <IconComponent
+                  data-status={status}
+                  className={` text-primary 
+                    ${status === 'active' ? 'text-primary!' : ''} 
+                    ${
+                      status === 'disabled'
+                        ? ' text-[#585858]! cursor-not-allowed'
+                        : ''
+                    }`}
+                />
               ) : (
                 <span>?</span>
               )}
             </AvatarFallback>
           </Avatar>
-          <p className="font-normal text-xl">{service.name}</p>
+          <p className="font-normal text-xl ">{service.name}</p>
         </CardContent>
       </Card>
 
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent>
+          <div>
+            <Avatar>
+              <AvatarFallback className="bg-white">
+                {IconComponent ? (
+                  <IconComponent
+                    data-status={status}
+                    className={` text-primary bg-accent
+                  
+                    }`}
+                  />
+                ) : (
+                  <span>?</span>
+                )}
+              </AvatarFallback>
+            </Avatar>
+          </div>
           <DialogHeader>
             <DialogTitle>Detalhes sobre {service.name}</DialogTitle>
           </DialogHeader>
--- src/assets/svgs/health-coaching.tsx
+++ src/assets/svgs/health-coaching.tsx
@@ -8,11 +8,12 @@ function HealthCoachingSVG(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 35 34"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <title>Health Coaching Icon</title>
       <path
         d="M15.2556 26.4449C16.3811 22.3445 20.3165 19.4157 24.8496 19.8999C29.1654 20.3607 32.5963 24.0237 32.8136 28.3818C32.868 29.53 32.7127 30.6312 32.379 31.6543C32.1771 32.2792 31.5717 32.6931 30.9119 32.6931H7.7946C3.87625 32.6931 0.937489 29.0856 1.70594 25.2195L6.43001 1.45209H15.7446L18.8495 6.91927L12.1973 11.707L10.3111 9.26234M12.2051 11.707L15.7446 24.8829"
-        stroke="#736CCE"
+        stroke="currentColor"
         stroke-width="2.51295"
         stroke-miterlimit="2"
         stroke-linecap="round"
