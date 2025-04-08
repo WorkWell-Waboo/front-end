7ded146bd427a9e32cb7add4f09d67afade23c42
rodrigo7584 <rodrigo.gandhi.oliveira@gmail.com>
2025-04-08 19:57:23 -0300

    schedules-collaborator

--- src/app/(application)/collaborator/schedules/page.tsx
+++ src/app/(application)/collaborator/schedules/page.tsx
@@ -1,7 +1,30 @@
+
+import {
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+ 
+
 function CollaboratorSchedulesPage() {
   return (
-    <main>
-      <h1>Meus Agendamento</h1>
+    <main className="p-10 flex flex-col gap-4">
+      
+       <Tabs defaultValue="next-sessions" className="w-[400px]">
+          <TabsList className="flex gap-5">
+            <TabsTrigger value="next-sessions">Próximas sessões</TabsTrigger>
+            <TabsTrigger value="history">Histórico</TabsTrigger>
+          </TabsList>
+          <TabsContent value="next-sessions">
+            <h1>teste</h1>
+          </TabsContent>
+          <TabsContent value="history">
+            <h1>teste2</h1>
+          </TabsContent>
+        </Tabs>
     </main>
   );
 }
