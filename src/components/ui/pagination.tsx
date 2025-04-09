c63b0e83c0e202d44654f520ee1ebe4dd809a620
rodrigo7584 <rodrigo.gandhi.oliveira@gmail.com>
2025-04-09 20:19:29 -0300

    add pagination

---
+++ src/components/ui/pagination.tsx
@@ -0,0 +1,120 @@
+
+
+import { EllipsisIcon } from "@/assets/svgs/ellipsis"
+import { RoundedArrow } from "@/assets/svgs/roundedArrow"
+
+import type { Button, } from "@/components/ui/button"
+import { cn } from "@/libraries/utils"
+
+function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
+  return (
+    <nav
+      aria-label="pagination"
+      data-slot="pagination"
+      className={cn("mx-auto flex w-full justify-center", className)}
+      {...props}
+    />
+  )
+}
+
+function PaginationContent({
+  className,
+  ...props
+}: React.ComponentProps<"ul">) {
+  return (
+    <ul
+      data-slot="pagination-content"
+      className={cn("flex flex-row items-center", className)}
+      {...props}
+    />
+  )
+}
+
+function PaginationItem({ ...props }: React.ComponentProps<"li">) {
+  return <li data-slot="pagination-item" {...props} />
+}
+
+type PaginationLinkProps = {
+  isActive?: boolean
+} & Pick<React.ComponentProps<typeof Button>, "size"> &
+  React.ComponentProps<"a">
+
+function PaginationLink({
+  className,
+  isActive,
+  size = "icon",
+  ...props
+}: PaginationLinkProps) {
+  return (
+    <a
+      aria-current={isActive ? "page" : undefined}
+      data-slot="pagination-link"
+      data-active={isActive}
+      className={cn(
+        "flex items-center justify-center w-[30px] h-[30px] text-[#4F4F4F] rounded-md hover:bg-primary hover:text-white text-sm",
+        isActive && "bg-primary text-white", 
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function PaginationPrevious({
+  className,
+  ...props
+}: React.ComponentProps<typeof PaginationLink>) {
+  return (
+    <PaginationLink
+      aria-label="Go to previous page"
+      size="default"
+      className={cn("hover:text-primary hover:bg-transparent mr-2", className)}
+      {...props}
+    >
+      <RoundedArrow className="rotate-180" />
+    </PaginationLink>
+  )
+}
+
+function PaginationNext({
+  className,
+  ...props
+}: React.ComponentProps<typeof PaginationLink>) {
+  return (
+    <PaginationLink
+      aria-label="Go to next page"
+      size="default"
+      className={cn("hover:text-primary hover:bg-transparent ml-2", className)}
+      {...props}
+    >
+      <RoundedArrow />
+    </PaginationLink>
+  )
+}
+
+function PaginationEllipsis({
+  className,
+  ...props
+}: React.ComponentProps<"span">) {
+  return (
+    <span
+      aria-hidden
+      data-slot="pagination-ellipsis"
+      className={cn("flex size-9 items-center justify-center", className)}
+      {...props}
+    >
+      <EllipsisIcon className="text-[#4F4F4F]"/>
+      <span className="sr-only">Mais</span>
+    </span>
+  )
+}
+
+export {
+  Pagination,
+  PaginationContent,
+  PaginationLink,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
+}
