da3ac34ce64034d859cec0373eb3bbda09e87f7c
maycoantony <marcosantonio50050@gmail.com>
2025-05-13 16:13:22 -0400

    commit: automatic

--- src/components/contentCalendar/calendarStats.tsx
+++ src/components/contentCalendar/calendarStats.tsx
@@ -24,6 +24,7 @@ export function CalendarStats() {
         </div>
 
         <Button
+          variant={'outline'}
           className="h-full items-center gap-2 rounded-lg bg-white hover:bg-[#e6e6e1] px-4 py-2 text-sm font-normal text-[#333333] shadow-none"
           onClick={() => setShowModal(true)}
         >
